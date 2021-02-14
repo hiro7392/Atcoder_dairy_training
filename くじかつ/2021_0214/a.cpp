279277702eaece63acfa5e0166ddede33320c7b0
fish0504 <k_hiro@kzd.biglobe.ne.jp>
2021-02-14 23:17:42 +0900

    0214

---
+++ くじかつ/2021_0214/a.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+#define rep(i,N) for(int i=0;i<N;i++)
+#define rep2(i,N) for(int i=1;i<=N;i++)
+using namespace std;
+long long  INF=1e18;
+long long mod=1e9+7;
+
+long long GCD(long long a, long long b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    if (b == 0) return a;
+    else return GCD(b, a % b);
+}
+long long modinv(long long a, long long m) {
+    long long b = m, u = 1, v = 0;
+    while (b) {
+        long long t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+#define debug 0
+
+int main(){
+    bool ans[4]={};
+    int a,b;
+    cin>>a>>b;
+    ans[a]=ans[b]=true;
+    rep2(i,3){
+        if(!ans[i]){
+            cout<<i<<endl;
+            return 0;
+        }
+    }
+}
+/*
+
+
+*/
