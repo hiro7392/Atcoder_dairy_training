ef71bd7b00d292249215385ad7898176279b1f36
川原大宙 <k_hiro@kzd.biglobe.ne.jp>
2022-01-25 16:04:18 +0900

    c問題をAC

--- ABC/235/c.cpp
+++ ABC/235/c.cpp
@@ -12,11 +12,37 @@ int dy[4]={0,1,0,-1};
 #define debug 0
 
 int main(){
+    int n,m;
+    cin>>n>>m;
+
+    vector<string>stations(n);
+    set<string>stopStations;
+
+    rep(i,n){
+        string s;
+        cin>>s;
+        stations[i]=s;
+    }
+
+    rep(i,m){
+        string s;
+        cin>>s;
+        stopStations.insert(s);
+    }
+    rep(i,n){
+
+        auto it=stopStations.find(stations[i]);
+
+        if(it!=stopStations.end()){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
     
 }
 /*
-
-//縦21 横64
 
 
 
