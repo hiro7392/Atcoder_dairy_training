7affaae1034af697f1a0376cd42f0a7e49905a61
川原大宙 <k_hiro@kzd.biglobe.ne.jp>
2022-02-13 00:37:54 +0900

    mod 360を維持しながら配列に記録して、ソート後差分を取る

--- ABC/238/b.cpp
+++ ABC/238/b.cpp
@@ -11,7 +11,40 @@ int dy[4]={0,1,0,-1};
 #define debug 0
 
 int main(){
-    
+    int n;
+    cin>>n;
+
+    vector<int> angles(n+1);
+    int nowPos=0;   //現在の角度
+    angles[0]=0;    //最初の切れ目
+    rep2(i,n){   
+        int nowAngle;
+        cin>>nowAngle;
+        nowPos+=nowAngle;
+        if(nowPos>360)nowPos-=360;
+        angles[i]=nowPos;
+#if debug
+        cout<<"nowPos "<<nowPos<<endl;
+        cout<<"angles "<<angles[i]<<endl;
+#endif 
+        
+        
+    }
+    sort(angles.begin(),angles.end());
+
+#if debug
+    cout<<"angle state\n";
+    rep(i,n+1)cout<<angles[i]<<" ";
+    cout<<endl;
+#endif
+    int ans=0;
+    rep2(i,n){
+        ans=max(ans,angles[i]-angles[i-1]);
+#if debug
+        cout<<ans<<endl;
+#endif
+    }
+    cout<<max(ans,360-angles[n])<<endl;
 }
 /*
 
