f38d19ed9be1aefa1b178e7335670ae3cc3bd0b4
川原大宙 <k_hiro@kzd.biglobe.ne.jp>
2022-02-19 23:24:25 +0900

    rootの中身を先にかけてからsqrtをとる

---
+++ ABC/239/a.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+#define rep(i,N) for(int i=0;i<N;i++)
+#define rep2(i,N) for(int i=1;i<=N;i++)
+using namespace std;
+long long  INF=1e18;
+long long mod=1e9+7;
+
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+#define debug 0
+
+int main(){
+    double x;
+    cin>>x;
+    double st=12800000;
+    double ans=x*(st+x);
+    //cout<<ans<<endl;
+    printf("%.6lf\n",sqrt(ans));
+    // cout<<x+sqrt(st)*sqrt(x)<<endl;
+    //cout<<sqrt(st)<<endl;
+}
+/*
+
+
+*/
