4e04b24985cd504c4c35d7155b21061612dbc540
川原大宙 <k_hiro@kzd.biglobe.ne.jp>
2022-03-09 12:54:11 +0900

    ((s-2a)&a) ==0を満たすならOK

--- ABC/238/d.cpp
+++ ABC/238/d.cpp
@@ -10,8 +10,27 @@ int dx[4]={1,0,-1,0};
 int dy[4]={0,1,0,-1};
 #define debug 0
 
+/*
+aとsの大きさに注意して
+((s-2a)&a) ==0を満たすならOK
+
+*/
+
 int main(){
-    
+    int t;
+    cin>>t;
+
+    rep(i,t){
+        long long a,s;
+        cin>>a>>s;
+        if(s<2*a){
+            cout<<"No"<<endl;
+        }else if(((s-2*a)&a)==0){
+            cout<<"Yes"<<endl;
+        }else{
+            cout<<"No"<<endl;
+        }
+    }
 }
 /*
 
